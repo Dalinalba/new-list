a6058ba298cb2c69dc5388177e0bf88c98510bd5
Dalinalba <148215916+Dalinalba@users.noreply.github.com>
2023-11-27 15:00:04 +0100

    install add app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+import { Context } from './Context/Context'
+
+function App() {
+  return (
+    <Context.Provider>
+    <div>
+    </div>
+    </Context.Provider>
+  )
+}
+
+export default App
